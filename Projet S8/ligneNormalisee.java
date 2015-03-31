3c072e95d32d996d7c5bde4b38fb6ae4f3d74bcf
Aurelien <aurelien.pignolet@gmail.com>
2015-03-31 16:17:06 +0200

    MAJ code

--- Projet S8/ligneNormalisee.java
+++ Projet S8/ligneNormalisee.java
@@ -1,101 +1,108 @@
-
-/**
- * Write a description of class ligneNormalisee here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class ligneNormalisee
-{
-    public static void main(String[] args)
-    {
-        String test1 = "papa a vu le fifi de lolo .";
-        char[]test=new char[test1.length()];
-        for(int i = 0; i < test1.length();i++){
-            test[i]=test1.charAt(i);
-        }
+public class ligneNormalisee {
+    public static void main(String[] args) {
+        char[]test = "papa a vu le fifi de lolo .".toCharArray();;
         display(test);
-        char[] result = ligneNormalisee(test,28);
-        if(result != null)
-        {
+        char[] result = ligneNormalisee(test,40);
+        if(result != null) {
             display(result);
         }
     }
 
-    public static char[] ligneNormalisee(char[] ligne, int l)
-    {
-        int nombreChar = numChar(ligne);
-        if(nombreChar>l)
-        {
-            System.out.println("More char than space.");
+    public static char[] ligneNormalisee(char[] ligne, int l) {
+        final int nombreChars = numChar(ligne);
+        final int nombreMots = numWord(ligne);
+        final int nombreEspacesParMot = checkPre(nombreChars, nombreMots, l);
+        if(nombreEspacesParMot == -1) {
             return null;
         }
-        int nombreMot = numWord(ligne);
-        int nombreEspaceParMot = (l-nombreChar)/(nombreMot-1);
-        int moreEspace = l-nombreChar-nombreEspaceParMot*(nombreMot-1);
+        int moreEspace = (l-nombreChars)%(nombreMots-1);
+        System.out.println("("+l+"-"+nombreChars+")/("+nombreMots+"-1)="+nombreEspacesParMot);
         char[] newTab = new char[l];
         int j = 0;
         boolean insertSpace=false;
-        for(int i = 0; i < ligne.length;i++) {
+        int i = 0;
+        while(i < ligne.length) {
             if(ligne[i] != ' ') {
                 newTab[j] = ligne[i];
                 insertSpace = false;
                 j++;
             }
             else if(insertSpace!=true) {
-                for(int p = 0;p < nombreEspaceParMot;p++) {
-                    newTab[j] = ' ';
-                    j++;
-                }
-                if(moreEspace > 0) {
-                    newTab[j] = ' ';
-                    moreEspace--;
-                    j++;
-                }
-                j--;
+                j = funInsertSpace(newTab, nombreEspacesParMot, j, moreEspace);
+                moreEspace--;
                 insertSpace = true;
-                j++;
             }
+            i++;
         }
         return newTab;
     }
     
-    public static int numChar(char[] ligne)
-    {
+     public static int numChar(char[] ligne) {
         int numChar = 0;
-        for(int i = 0; i < ligne.length;i++)
-        {
-            if(ligne[i] != ' ')
-            {
+        int i = 0;
+        while (i < ligne.length) {
+            if(ligne[i] != ' ') {
                 numChar++;
             }
+            i++;
         }
         return numChar;
     }
     
-    public static int numWord(char[] ligne)
-    {
+    public static int numWord(char[] ligne) {
         int numWord = 0;
-        if(ligne[0] != ' ')
-        {
+        int i = 0;
+        if(ligne[0] != ' ') {
             numWord++;
         }
-        for(int i = 0; i < ligne.length-1;i++)
-        {
-            if(ligne[i] == ' ' && ligne[i+1] != ' ')
-            {
+        while( i < ligne.length-1) {
+            if(ligne[i] == ' ' && ligne[i+1] != ' ') {
                 numWord++;
             }
-        }
-        
+            i++;
+        }   
         return numWord;
     }
     
-    public static void display(char[] ligne)
-    {
-        for(int i = 0; i < ligne.length;i++)
+    public static int checkPre(int nombreChars, int nombreMots, int l) {
+        if(nombreChars > l) {
+            System.out.println("More char than the length of the new word.");
+            return -1;
+        }   
+        if(nombreMots == 1) {
+            if(nombreChars != l) {
+                System.out.println("Impossible, because we have to insert space at the end of the line");
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        }
+        else
         {
-            System.out.print(ligne[i]);         
+             return (l-nombreChars)/(nombreMots-1);
+        }
+    }
+    
+    public static int funInsertSpace(char[] newTab, int nombreEspaceParMot, int j, int moreEspace) {
+        int jFinal=j+nombreEspaceParMot;
+        while(j < jFinal) {
+            newTab[j] = ' ';
+            j++;
+        }
+        if(moreEspace > 0) {
+            newTab[j] = ' ';
+            moreEspace--;
+            j++;
+        }
+        return j;
+    }
+    
+    public static void display(char[] ligne) {
+        int i = 0;
+        while(i < ligne.length) {
+            System.out.print(ligne[i]);   
+            i++;
         }
         System.out.println();
     }
